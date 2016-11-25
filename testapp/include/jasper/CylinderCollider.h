a049002a2844b46fd673b23e500d38767ad103d9
Allan Irvine <alirvine@gmail.com>
2016-11-25 09:49:46 -0500

    mark const methods const

--- testapp/include/jasper/CylinderCollider.h
+++ testapp/include/jasper/CylinderCollider.h
@@ -9,7 +9,7 @@ class CylinderCollider : public PhysicsCollider
 public:
     explicit CylinderCollider(const std::string& name, const Vector3& halfExtents, PhysicsWorld* world);
     void Awake() override;
-    ComponentType GetComponentType() override {
+    ComponentType GetComponentType() const override {
         return ComponentType::CylinderCollider;
     }
 };
